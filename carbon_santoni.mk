fee14b2205fdbd0b12454d6b21c1500ef39f3e99
Nich <nctrenco@gmail.com>
2019-04-22 11:19:39 +0800

    santoni: Switch to V9.5.10.0.NAMMIFD build fingerprint
    This one passes CTS.
    
    Change-Id: Ibe4e05305a0ee8593d1dbfb61acc09218f25079d

--- carbon_santoni.mk
+++ carbon_santoni.mk
@@ -45,6 +45,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := santoni
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="santoni-user 7.1.2 N2G47H V9.2.1.0.NAMCNEK release-keys"
+    PRIVATE_BUILD_DESC="santoni-user 7.1.2 N2G47H V9.5.10.0.NAMMIFD release-keys"
 
-BUILD_FINGERPRINT := Xiaomi/santoni/santoni:7.1.2/N2G47H/V9.2.1.0.NAMCNEK:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/santoni/santoni:7.1.2/N2G47H/V9.5.10.0.NAMMIFD:user/release-keys
