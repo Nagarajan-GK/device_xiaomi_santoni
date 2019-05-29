beb574d69c20fa4e40d393880fcf7a1e118b2bf4
ErrorNetwork28 <zidan23.05.2016@gmail.com>
2019-05-29 17:47:32 +0000

    CAF bring up!

--- aosp_santoni.mk
+++ aosp_santoni.mk
@@ -12,7 +12,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)#
 
 $(call inherit-product, device/xiaomi/santoni/full_santoni.mk)
 
